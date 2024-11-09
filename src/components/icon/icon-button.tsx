6a1689258eb852bb9e8a0c322c1f04c89f0a500d
tusong <tusong1126@foxmail.com>
2024-11-09 22:16:58 +0800

    feat: 新增classnames、@iconify/react

---
+++ src/components/icon/icon-button.tsx
@@ -0,0 +1,19 @@
+import { CSSProperties, ReactNode } from 'react'
+import { ButtonProps } from 'antd'
+
+type Props = {
+  children: ReactNode
+  className?: string
+  style?: CSSProperties
+} & ButtonProps
+export default function IconButton({ children, className, style, onClick }: Props) {
+  return (
+    <button
+      style={style}
+      className={`flex cursor-pointer items-center justify-center rounded-full p-2 hover:bg-hover ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
