253f8edf183169abaaeb0d93f611189a445e80a4
tusong <tusong1126@foxmail.com>
2024-11-10 15:18:07 +0800

    feat: 路由及布局改造

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
+import { Helmet } from 'react-helmet-async'
+import { App as AntdApp, ConfigProvider } from 'antd'
 
-import viteLogo from '/vite.svg'
-
-import reactLogo from './assets/svg/react.svg'
+import { MotionLazy } from '@/components/basic/animate/motion-lazy'
+import Router from '@/router/index'
+import Logo from '@/assets/images/home.png'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <ConfigProvider>
+      <AntdApp>
+        <MotionLazy>
+          <Helmet>
+            <title>Accompany</title>
+            <link rel="icon" href={Logo} />
+          </Helmet>
+
+          <Router />
+        </MotionLazy>
+      </AntdApp>
+    </ConfigProvider>
   )
 }
 
--- src/components/errorBoundary.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouteError } from 'react-router-dom'
-
-export default function ErrorBoundary() {
-  const error = useRouteError()
-  //错误信息，可用来错误上报
-  // eslint-disable-next-line no-console
-  console.log(error)
-  return <>错误页面</>
-}
