b54cc8dc8528bcf69932c090779d481f2ab97922
tusong <tusong1126@foxmail.com>
2024-11-10 15:45:29 +0800

    feat: 文件目录变更

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,8 @@
-import { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
 
 import App from '@/App'
 import '@/styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(
-  <HelmetProvider>
-    <Suspense>
-      <App />
-    </Suspense>
-  </HelmetProvider>
-)
+root.render(<App />)
--- src/router/auth-guard.tsx
+++ src/router/auth-guard.tsx
@@ -1,18 +1,18 @@
 import { lazy, useCallback, useEffect } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
-import { useRouter } from '@/hooks/use-router'
+import { useRouter } from '@/hooks/Basic/useRouter'
 
 type Props = {
   children: React.ReactNode
 }
+
 const ErrorPage = lazy(() => import('@/views/ErrorPage'))
 
 export default function AuthGuard({ children }: Props) {
   const router = useRouter()
 
   const accessToken = localStorage.getItem('token')
-  console.log(1111111, '切换路由，检验是否带有accessToken', accessToken)
 
   const check = useCallback(() => {
     if (!accessToken) {
