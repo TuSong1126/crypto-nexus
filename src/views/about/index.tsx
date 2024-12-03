84546e8380a028e3e0c9b16b4854a480f8536266
tusong <tusong1126@foxmail.com>
2024-12-03 22:19:00 +0800

    feat：退出按钮

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,12 +1,26 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd'
+
+import useUserInfoStore from '@/store/userInfo'
 
 import { CircleLoading } from '@/components/basic/circle-loading'
+import { ConstEnum } from '@/enums'
 
 export default function AboutPage() {
+  const userInfoStore = useUserInfoStore()
+
+  const logout = () => {
+    localStorage.removeItem(ConstEnum.TOKEN)
+    userInfoStore.updateToken('')
+  }
+
   return (
     <>
       <div>关于</div>
+      <Button type="primary" onClick={logout}>
+        退出
+      </Button>
       <br />
       <Suspense fallback={<CircleLoading />}>
         <Outlet />
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,22 +1,7 @@
-import { Button } from 'antd'
-
-import useUserInfoStore from '@/store/userInfo'
-
-import { ConstEnum } from '@/enums'
-
 export default function Home() {
-  const userInfoStore = useUserInfoStore()
-
-  const logout = () => {
-    localStorage.removeItem(ConstEnum.TOKEN)
-    userInfoStore.updateToken('')
-  }
-
   return (
     <>
-      <Button type="primary" onClick={logout}>
-        退出
-      </Button>
+      <div>首页</div>
     </>
   )
 }
