0451a527e43167b86e28d53f0659d146ea4a1e36
tusong <tusong1126@foxmail.com>
2024-12-03 22:02:41 +0800

    feat: logo点击到首页

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,6 +3,8 @@ import { NavLink, Outlet } from 'react-router-dom'
 
 import './index.scss'
 
+import { useRouter } from '@/hooks/basic/useRouter'
+
 import { CircleLoading } from '@/components/basic/circle-loading'
 import businessRoutes from '@/router/routes/index'
 
@@ -11,11 +13,14 @@ const { VITE_APP_HOMEPAGE } = import.meta.env
 export default function Layout() {
   // 首页不在菜单中展示(注意斜杠)
   const menuList = businessRoutes.filter((i) => !VITE_APP_HOMEPAGE.includes(i.meta?.permissionKey))
+  const router = useRouter()
 
   return (
     <div className="app-layout-wrapper">
       <div className="header-wrapper tool-box-shadow">
-        <div className="logo">LOGO</div>
+        <div className="logo" onClick={() => router.push(VITE_APP_HOMEPAGE)}>
+          LOGO
+        </div>
 
         <div className="menu">
           {menuList.map((item) => (
