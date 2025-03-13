4fac5fe274d69d5bd2cf9bb6888e29720f0ebfe0
tusong <ts2@yoc.tech>
2025-03-14 01:38:30 +0800

    feat: 移除strict

--- src/components/basic/animate/motion-lazy.tsx
+++ src/components/basic/animate/motion-lazy.tsx
@@ -8,7 +8,7 @@ type Props = {
  */
 export function MotionLazy({ children }: Props) {
   return (
-    <LazyMotion strict features={domMax}>
+    <LazyMotion features={domMax}>
       <m.div style={{ height: '100%' }}> {children} </m.div>
     </LazyMotion>
   )
