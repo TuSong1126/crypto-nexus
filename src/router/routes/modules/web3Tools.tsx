af9568d46d5a10aa309b48f26db090201b880bdf
tusong <tusong1126@foxmail.com>
2025-04-06 08:55:32 +0800

    feat: 多余路由移除

--- src/router/routes/modules/web3Tools.tsx
+++ src/router/routes/modules/web3Tools.tsx
@@ -10,21 +10,6 @@ const web3ToolsRoutes: RouteType[] = [
         path: 'wallet',
         Component: lazy(() => import('@/views/web3Tools/wallet/index')),
         meta: { title: '钱包管理', permissionKey: 'web3Tools/wallet', icon: '' }
-      },
-      {
-        path: 'explorer',
-        Component: lazy(() => import('@/views/web3Tools/explorer/index')),
-        meta: { title: '区块浏览器', permissionKey: 'web3Tools/explorer', icon: '' }
-      },
-      {
-        path: 'demo',
-        Component: lazy(() => import('@/pages/Web3Demo')),
-        meta: { title: 'Web3演示', permissionKey: 'web3Tools/demo', icon: '' }
-      },
-      {
-        path: 'transaction',
-        Component: lazy(() => import('@/views/web3Transaction')),
-        meta: { title: '链上交易', permissionKey: 'web3Tools/transaction', icon: '' }
       }
     ]
   }
