139c2ffffc5cbb37de1cf6186a10c3badaf159b3
tusong <tusong1126@foxmail.com>
2025-07-19 11:01:42 +0800

    feat: 处理启动的控制台警告问题

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import path, { resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import viteCompression from 'vite-plugin-compression'
-import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
+import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
