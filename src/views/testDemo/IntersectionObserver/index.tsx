d4f1e347dff1b54768c01122d5903040ead1b1be
tusong <tusong1126@foxmail.com>
2025-07-19 14:11:04 +0800

    feat: 使用IntersectionObserver，浏览器会利用渲染树和时事件循环机制自动完成这些工作，从而极大的简化可见性相关的开发工作

--- src/views/testDemo/IntersectionObserver/index.tsx
+++ src/views/testDemo/IntersectionObserver/index.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 
 import LazyChart from './LazyChart'
 
+// IntersectionObserver 的底层实现依赖于浏览器的渲染管线、几何计算和异步任务调度。
+// 它通过批量处理、异步回调和高效的交叉检测机制，提供了高性能的元素可见性监控功能。
+// 开发者无需手动计算元素位置，浏览器会利用渲染树和事件循环自动完成这些工作，从而极大地简化了可见性相关的开发任务。
+
 const App: React.FC = () => {
   const chartOptions: EChartsOption = {
     xAxis: {
